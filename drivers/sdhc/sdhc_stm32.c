a4b533aca1326ea3cdeca22d217fba7f125d2ca4
ExaltZephyr <zephyr@exalt123>
2025-02-17 11:37:23 +0200

    Complete driver APIs

--- drivers/sdhc/sdhc_stm32.c
+++ drivers/sdhc/sdhc_stm32.c
@@ -18,12 +18,13 @@
 LOG_MODULE_REGISTER(sdhc_stm32, CONFIG_SDHC_LOG_LEVEL);
 typedef void (*irq_config_func_t)(const struct device *port);
 
-#define SD_TIMEOUT   ((uint32_t)0x00100000U)
+#define SD_TIMEOUT        ((uint32_t)0x00100000U)
+#define SDHC_CMD_TIMEOUT  K_MSEC(200)
 
 struct sdhc_stm32_config {
-  uint8_t                           bus_width;
   uint16_t                          clk_div;
   bool                              hw_flow_control;
+  uint32_t                          power_delay_ms;
 	const struct stm32_pclken        *pclken;              /* Pointer to peripheral clock configuration */
 	const struct pinctrl_dev_config  *pcfg;               /* Pointer to pin control configuration */
   irq_config_func_t                 irq_config_func;    /* IRQ config function */
@@ -31,27 +32,10 @@ struct sdhc_stm32_config {
 
 struct sdhc_stm32_data {
   struct k_mutex     bus_mutex; 
-  SD_HandleTypeDef   hsd; 
-  uint32_t cmd_index;
+  SD_HandleTypeDef   hsd;
+  struct sdhc_io     host_io;
+  struct k_sem       cmd_sem;
 };
-
-static uint8_t Wait_SDCARD_Ready( struct sdhc_stm32_data *dev_data)
-{
-  uint32_t loop = SD_TIMEOUT;
-
-  /* Wait for the Erasing process is completed */
-  /* Verify that SD card is ready to use after the Erase */
-  while(loop > 0)
-  {
-    loop--;
-    if(HAL_SD_GetCardState(&dev_data->hsd) == HAL_SD_CARD_TRANSFER)
-    {
-      return HAL_OK;
-    }
-  }
-  return HAL_ERROR;
-}
-
 
 void sdhc_stm32_log_err_type(SD_HandleTypeDef *hsd)
 {
@@ -130,7 +114,6 @@ static int sdhc_stm32_write_blocks(const struct device *dev, struct sdhc_data *d
     int ret;
     struct sdhc_stm32_data *dev_data = dev->data;
 
-    k_mutex_lock(&dev_data->bus_mutex, K_FOREVER);
     __ASSERT(data != NULL, "Data is required for this command.");
 
     if(HAL_SD_Erase(&dev_data->hsd, data->block_addr, (data->block_addr+sizeof(data->data))) != HAL_OK)
@@ -138,8 +121,8 @@ static int sdhc_stm32_write_blocks(const struct device *dev, struct sdhc_data *d
       return HAL_ERROR;
     }
 
-    if(Wait_SDCARD_Ready(dev_data) != HAL_OK)
-    {
+    if(!WAIT_FOR(HAL_SD_GetCardState(&dev_data->hsd) == HAL_SD_CARD_TRANSFER, SD_TIMEOUT, k_usleep(1))) {
+      LOG_ERR("SD Card is busy now");
       return HAL_ERROR;
     }
 
@@ -148,6 +131,12 @@ static int sdhc_stm32_write_blocks(const struct device *dev, struct sdhc_data *d
     #else
       ret = HAL_SD_WriteBlocks_IT(&dev_data->hsd, data->data, data->block_addr, data->blocks);
     #endif
+
+    if (k_sem_take(&dev_data->cmd_sem, SDHC_CMD_TIMEOUT) != 0) {
+      LOG_ERR("Failed to acquire Semaphore\n");
+		  return -ETIMEDOUT;
+    }
+
     return ret;
 }
 
@@ -155,81 +144,66 @@ static int sdhc_stm32_read_blocks(const struct device *dev, struct sdhc_data *da
     int ret;
     struct sdhc_stm32_data *dev_data = dev->data;
 
-    k_mutex_lock(&dev_data->bus_mutex, K_FOREVER);
     #ifdef CONFIG_SDHC_STM32_DMA
         ret = HAL_SD_ReadBlocks_DMA(&dev_data->hsd, data->data, data->block_addr, data->blocks);
     #else
         ret = HAL_SD_ReadBlocks_IT(&dev_data->hsd, data->data, data->block_addr, data->blocks);
     #endif
 
+    if (k_sem_take(&dev_data->cmd_sem, SDHC_CMD_TIMEOUT) != 0) {
+      LOG_ERR("Failed to acquire Semaphore\n");
+		  return -ETIMEDOUT;
+    }
+
     return ret;
 }
 
 static uint32_t sdhc_stm32_go_idle_state(const struct device *dev){
-      uint32_t res;
+    uint32_t res;
     struct sdhc_stm32_data *dev_data = dev->data;
-      res = SDMMC_CmdGoIdleState(SDMMC1);
-      if (res != HAL_OK) {
-          sdhc_stm32_log_err_type(&dev_data->hsd);
-          return res;
-      }
 
-      res = HAL_SD_Init(&dev_data->hsd);
-      if (res != HAL_OK)
-      {
-          sdhc_stm32_log_err_type(&dev_data->hsd);
-          return res;
-      }
+    res = SDMMC_CmdGoIdleState(SDMMC1);
+    if (res != HAL_OK) {
+        sdhc_stm32_log_err_type(&dev_data->hsd);
+        return res;
+    }
 
-      return 0;
+    res = HAL_SD_Init(&dev_data->hsd);
+    if (res != HAL_OK)
+    {
+        sdhc_stm32_log_err_type(&dev_data->hsd);
+        return res;
+    }
+
+     if(!WAIT_FOR(HAL_SD_GetCardState(&dev_data->hsd) == HAL_SD_CARD_TRANSFER, SD_TIMEOUT, k_usleep(1))) {
+        LOG_ERR("SD Card is busy now");
+        return HAL_ERROR;
+     }
+
+    return 0;
 }
 
 static int sdhc_stm32_abort(const struct device *dev){
     int res;
     struct sdhc_stm32_data *data = dev->data;
-    #ifdef SDHC_STM32_POLLING
-      res = HAL_SD_Abort(&data->hsd);
-    #else
-      res = HAL_SD_Abort_IT(&data->hsd);
-    #endif
+    res = HAL_SD_Abort_IT(&data->hsd);
     return res;
 }
 
-static int sdhc_stm32_send_cid(const struct device *dev, struct sdhc_data *data){
-      int res;
-      struct sdhc_stm32_data *dev_data = dev->data;
-
-      HAL_SD_CardCIDTypeDef *data_pcid = (HAL_SD_CardCIDTypeDef *) data->data;
-      res = HAL_SD_GetCardCID(&dev_data->hsd, data_pcid);
-
-      return res;
-}
-
-static int sdhc_stm32_send_csd(const struct device *dev, struct sdhc_data *data){
-      int res;
-      struct sdhc_stm32_data *dev_data = dev->data;
-
-      HAL_SD_CardCSDTypeDef *data_csd= (HAL_SD_CardCSDTypeDef *) data->data;
-      res = HAL_SD_GetCardCSD(&dev_data->hsd, data_csd);
-
-      return res;
-}
-
 static int sdhc_stm32_erase_block(const struct device *dev, struct sdhc_data *data) {
-      int res;
-      struct sdhc_stm32_data *dev_data = dev->data;
-      k_mutex_lock(&dev_data->bus_mutex, K_FOREVER);
+    int res;
+    struct sdhc_stm32_data *dev_data = dev->data;
 
-      res = HAL_SD_Erase(&dev_data->hsd, data->block_addr, (data->block_size + data->block_addr));
-
-      if(res != HAL_OK) {
-        return res;
-      }
-
-      res = Wait_SDCARD_Ready(dev_data);
-
-      k_mutex_unlock(&dev_data->bus_mutex);
+    res = HAL_SD_Erase(&dev_data->hsd, data->block_addr, (data->block_size + data->block_addr));
+    if(res != HAL_OK) {
       return res;
+    }
+
+    if( !WAIT_FOR(HAL_SD_GetCardState(&dev_data->hsd) == HAL_SD_CARD_TRANSFER, SD_TIMEOUT, k_usleep(1))) {
+      LOG_ERR("SD Card is busy now");
+      return HAL_ERROR;
+    }
+    return res;
 }
 
 static int sdhc_stm32_request(const struct device *dev, struct sdhc_command *cmd, struct sdhc_data *data)
@@ -239,51 +213,40 @@ static int sdhc_stm32_request(const struct device *dev, struct sdhc_command *cmd
 
    __ASSERT(cmd != NULL, "Command is NULL.");
 
-    if(Wait_SDCARD_Ready(dev_data) != HAL_OK)
-    {
-      LOG_ERR("SD Card is busy now");
-      return HAL_ERROR;
-    }
+     if( !WAIT_FOR(HAL_SD_GetCardState(&dev_data->hsd) == HAL_SD_CARD_TRANSFER, SD_TIMEOUT, k_usleep(1))) {
+        LOG_ERR("SD Card is busy now");
+        return HAL_ERROR;
+     }
 
-    dev_data->cmd_index = cmd->opcode;
+    k_mutex_lock(&dev_data->bus_mutex, K_FOREVER);
 
 		switch (cmd->opcode) {
-        case SD_WRITE_SINGLE_BLOCK:
-        case SD_WRITE_MULTIPLE_BLOCK:
-          res = sdhc_stm32_write_blocks(dev, data);
-          break;
+      case SD_WRITE_SINGLE_BLOCK:
+      case SD_WRITE_MULTIPLE_BLOCK:
+        res = sdhc_stm32_write_blocks(dev, data);
+        break;
 
-        case SD_READ_SINGLE_BLOCK:
-        case SD_READ_MULTIPLE_BLOCK:
-          res = sdhc_stm32_read_blocks(dev, data);
-          break;
+      case SD_READ_SINGLE_BLOCK:
+      case SD_READ_MULTIPLE_BLOCK:
+        res = sdhc_stm32_read_blocks(dev, data);
+        break;
 
-        case SD_ERASE_BLOCK_OPERATION:
-          res = sdhc_stm32_erase_block(dev, data);
-          break;
-        case SD_GO_IDLE_STATE:
-          k_mutex_lock(&dev_data->bus_mutex, K_FOREVER);
-          res = sdhc_stm32_go_idle_state(dev);
-          k_mutex_unlock(&dev_data->bus_mutex);
-          break;
-        case SD_STOP_TRANSMISSION:
-          res = sdhc_stm32_abort(dev);
-          break;
-
-        case SD_SEND_CID:
-          res = sdhc_stm32_send_cid(dev, data);
-          break;
-
-        case SD_SEND_CSD:
-          res = sdhc_stm32_send_csd(dev, data);
-          break;
-
-        default:
-          res = HAL_ERROR;
-          LOG_ERR("Unsupported Command.");
-          break;
+      case SD_ERASE_BLOCK_OPERATION:
+        res = sdhc_stm32_erase_block(dev, data);
+        break;
+      case SD_GO_IDLE_STATE:
+        res = sdhc_stm32_go_idle_state(dev);
+        break;
+      case SD_STOP_TRANSMISSION:
+        res = sdhc_stm32_abort(dev);
+        break;
+      default:
+        res = HAL_ERROR;
+        LOG_ERR("Unsupported Command.");
+        break;
 		}
 
+    k_mutex_unlock(&dev_data->bus_mutex);
     if(res != HAL_OK) {
       sdhc_stm32_log_err_type(&dev_data->hsd);
     }
@@ -319,11 +282,73 @@ static int sdhc_stm32_card_busy(const struct device *dev)
 
 }
 
+static int sdhc_stm32_set_io(const struct device *dev, struct sdhc_io *ios)
+{
+	struct sdhc_stm32_data *data = dev->data;
+	struct sdhc_io *host_io = &data->host_io;
+  int res = 0;
+
+	if ((ios->bus_width != 0) && (host_io->bus_width != ios->bus_width)) {
+    res = HAL_SD_ConfigWideBusOperation(&data->hsd, (uint32_t) ios->bus_width);
+    //res =  HAL_SDIO_SetDataBusWidth(&data->hsd, (uint32_t) ios->bus_width);
+    if(res != HAL_OK) {
+      LOG_ERR("Failed to set data bus width as %d\n", ios->bus_width);
+    } else {
+      host_io->bus_width = ios->bus_width;
+    }
+	}
+
+  if ((ios->clock != 0) && ( host_io->clock !=ios->clock) )
+  {
+      res = HAL_SD_ConfigSpeedBusOperation(&data->hsd, (uint32_t) ios->clock);
+      //res = HAL_SDIO_ConfigFrequency(&data->hsd, (uint32_t) ios->clock);
+      if(res != HAL_OK) {
+        LOG_ERR("Failed to set clock as %d\n", ios->clock);
+      } else {
+        host_io->clock = ios->clock;
+      }
+  }
+
+  if ((ios->signal_voltage !=0 ) && (host_io->signal_voltage != ios->signal_voltage) &&  (ios->signal_voltage == SD_VOL_1_8_V ) ) {
+        res = SDMMC_CmdVoltageSwitch(data->hsd.Instance);
+
+        if(res != HAL_SD_ERROR_NONE) {
+          LOG_ERR("Failed to set signal voltage as %d\n", ios->signal_voltage);
+        } else {
+          host_io->signal_voltage = ios->signal_voltage;
+        }
+  }
+
+  return res;
+}
+
+static int sdhc_stm32_get_host_props(const struct device *dev, struct sdhc_host_props *props)
+{
+	const struct sdhc_stm32_config *sdhc_config = (struct sdhc_stm32_config *)dev->config;
+	memset(props, 0, sizeof(struct sdhc_host_props));
+
+	props->f_min = SD_CLOCK_25MHZ;
+	props->f_max = SD_CLOCK_208MHZ;
+	props->power_delay = sdhc_config->power_delay_ms;
+	props->host_caps.vol_330_support = true;
+
+	props->host_caps.vol_180_support = true;
+	props->host_caps.bus_8_bit_support = true;
+  props->host_caps.bus_4_bit_support = true;
+  props->host_caps.hs200_support = false;
+  props->host_caps.hs400_support = false;
+	props->is_spi = false;
+
+	return 0;
+}
+
 static const struct sdhc_driver_api sdhc_stm32_api = {
 	.request = sdhc_stm32_request,
 	.get_card_present = sdhc_stm32_get_card_present,
 	.reset = sdhc_stm32_reset,
 	.card_busy = sdhc_stm32_card_busy,
+  .set_io = sdhc_stm32_set_io,
+  .get_host_props = sdhc_stm32_get_host_props,
 };
 
 static int sdhc_stm32_activate(const struct device *dev)
@@ -383,14 +408,17 @@ int sdhc_stm32_sd_init(const struct device *dev)
     hsd->Init.HardwareFlowControl = SDMMC_HARDWARE_FLOW_CONTROL_DISABLE;
   }
 
-  if(config->bus_width == 4) {
+  if(data->host_io.bus_width == 4) {
     hsd->Init.BusWide = SDMMC_BUS_WIDE_4B;
-  } else if(config->bus_width == 8) {
+  } else if(data->host_io.bus_width == 8) {
     hsd->Init.BusWide = SDMMC_BUS_WIDE_8B;
   } else {
     hsd->Init.BusWide = SDMMC_BUS_WIDE_1B;
   }
-
+  // int res = HAL_SD_ConfigSpeedBusOperation(&data->hsd, data->host_io.clock);
+  // if(res != HAL_OK) {
+  //   LOG_ERR("Configure Speed Failed.");
+  // }
   return HAL_SD_Init(hsd);
 
 }
@@ -410,6 +438,8 @@ static int sdhc_stm32_init(const struct device *dev)
   }
 
   k_mutex_init(&data->bus_mutex);
+  k_sem_init(&data->cmd_sem, 0, K_SEM_MAX_LIMIT);
+
   config->irq_config_func(dev);
 
 	return 0;
@@ -419,19 +449,17 @@ static void i3c_stm32_event_isr(void *arg)
 {
     const struct device     *dev = (const struct device *)arg;
     struct sdhc_stm32_data  *data = dev->data;
-    uint32_t error_code = HAL_SD_GetError(&data->hsd);
 
-    HAL_SD_IRQHandler(&data->hsd); 
-
-    if (((data->hsd.Context & SD_CONTEXT_READ_SINGLE_BLOCK) != 0U) || ((data->hsd.Context & SD_CONTEXT_READ_MULTIPLE_BLOCK) != 0U) ||
-        ((data->hsd.Context & SD_CONTEXT_WRITE_SINGLE_BLOCK) != 0U) || (data->hsd.Context & SD_CONTEXT_WRITE_MULTIPLE_BLOCK) != 0U) {
-       k_mutex_unlock(&data->bus_mutex);
+    if(__HAL_SD_GET_FLAG(&data->hsd, SDMMC_FLAG_DATAEND)) {
+        k_sem_give(&data->cmd_sem);
     }
 
-    if(((error_code != HAL_SD_ERROR_NONE)  && (data->cmd_index != SD_STOP_TRANSMISSION))) {
-       k_mutex_unlock(&data->bus_mutex);
+    if (__HAL_SD_GET_FLAG(&data->hsd, SDMMC_FLAG_DCRCFAIL | SDMMC_FLAG_DTIMEOUT | SDMMC_FLAG_RXOVERR | SDMMC_FLAG_TXUNDERR) || (data->hsd.ErrorCode != HAL_SD_ERROR_NONE))
+    {
+        k_sem_give(&data->cmd_sem);
     }
 
+    HAL_SD_IRQHandler(&data->hsd);
 }
 
 #define STM32_SDHC_IRQ_CONNECT_AND_ENABLE(index)                                                    \
@@ -462,11 +490,15 @@ static void i3c_stm32_event_isr(void *arg)
 		.pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(index),                                     \
     .hw_flow_control= DT_INST_PROP_OR(index, hw_flow_control,0),	\
     .clk_div= DT_INST_PROP_OR(index, clk_div,4),	\
-    .bus_width= DT_INST_PROP(index, bus_width),    \
+    .power_delay_ms = DT_INST_PROP_OR(inst, power_delay_ms, 1),	\
 	};                                                                                         \
 	static struct sdhc_stm32_data sdhc_stm32_data_##index = {                             \
     .hsd = {   \
       .Instance = (MMC_TypeDef *)DT_INST_REG_ADDR(index),   \
+    },   \
+    .host_io = {   \
+      .bus_width= DT_INST_PROP_OR(index, bus_width, 4),    \
+      .clock= DT_INST_PROP_OR(index, clock_frequency, MHZ(12.5) ),    \
     }   \
 	};                                                                                         \
 	DEVICE_DT_INST_DEFINE(index,					\
